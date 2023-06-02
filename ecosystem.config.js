1f23c1f7afb5d922556522ad441f6c1ad484abba
robot <robot@Gabrielas-MacBook-Air.local>
2023-06-02 22:28:52 +0200

    pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
-export default {
+module.exports = {
   apps: [
     {
       name: 'db-api',
       script: './index.js',
-      instances: 'max',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+      instances: 'max', // to use maximum available CPUs
+      autorestart: true, // to restart app if it crashes
+      watch: false, // to watch for file changes and restart if any change
+      max_memory_restart: '1G', // if the app reaches 1GB memory it will restart
       env: {
         NODE_ENV: 'development'
       },
