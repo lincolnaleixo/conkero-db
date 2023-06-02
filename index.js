e5ab08fe3741ac93664fa660805b03bf665075c0
robot <robot@Gabrielas-MacBook-Air.local>
2023-06-02 22:19:42 +0200

    permissions and much more

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import apiKeysRoutes from './routes/apiKeys.js';
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import { checkPermissions } from './middleware/checkPermissions.js'
+import { errorHandler } from './middleware/errorHandler.js'
+import logger from './middleware/logger.js'
+import { validateApiKey } from './middleware/validateApiKey.js'
+import keysRoutes from './routes/keys.js'
 
-const app = express();
+dotenv.config()
 
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
 
-app.use('/api-keys', apiKeysRoutes);
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(logger) // Use the logger middleware
+app.use(errorHandler) // Use the error handler middleware
+
+app.use(validateApiKey)
+app.use(checkPermissions)
+app.use('/keys', keysRoutes)
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected…'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected…'))
+  .catch(err => console.log(err))
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is running on port ${port}`))
